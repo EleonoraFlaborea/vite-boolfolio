a523b7d30aa2ecbdc7032ac62b8f1b602a8d00d8
EleonoraFlaborea <flaboreaeleonora@gmail.com>
2024-03-29 11:13:33 +0100

    add projectcard components

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,32 @@
 <script>
+import axios from 'axios';
+import AppHeader from '.components/AppHeader.vue';
+import ProjectCard from '.components/projects/ProjectCard.vue';
+const endpoint = 'http://localhost:8000/api/projects/';
+
 export default {
   name: 'Boolfolio',
-  components: { AppHeader }
+  components: { AppHeader, ProjectCard },
+  data: () => ({ projects: [] }),
+  methods: {
+    fetchProjects() {
+      axios.get(endpoint).then(res => {
+        console.log(res.data);
+
+        this.projects = res.data;
+      })
+    }
+  },
+  created() {
+    this.fetchProjects();
+  }
 };
 </script>
 
 <template>
   <AppHeader />
-  <main class="container pt-4">
-    <h1>Boolfolio</h1>
+  <main>
+    <ProjectCard />
   </main>
 
 </template>
---
+++ src/components/projects/ProjectCard.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    name: 'ProjectCard',
+};
+</script>
+
+<template>
+
+    <section class="container pt-4">
+        <h1>Boolfolio</h1>
+
+        <ul>
+            <li v-for="project in projects" :key="project.id">
+                <h3 v-text="project.title"></h3>
+            </li>
+        </ul>
+    </section>
+
+</template>
+
+<style scoped lang="scss"></style>
