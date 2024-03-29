d9b7d16ad0767da2a807254ab8efb4aaf6e5216e
EleonoraFlaborea <flaboreaeleonora@gmail.com>
2024-03-29 10:56:53 +0100

    add appheader

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <script>
-
+export default {
+  name: 'Boolfolio',
+  components: { AppHeader }
+};
 </script>
 
 <template>
+  <AppHeader />
+  <main class="container pt-4">
+    <h1>Boolfolio</h1>
+  </main>
 
 </template>
 
